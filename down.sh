ab632b1e556aca0ae1e489185c7c81dd02256bcc
Ray Kochenderfer <rayjkochenderfer@gmail.com>
2022-04-13 15:58:18 -0400

    Add logging

--- down.sh
+++ down.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 docker-compose -f deployments/development.yaml down
+# delete images
+docker image rm deployments_server
+docker image rm deployments_frontend
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,47 @@ import { serve } from 'https://deno.land/std@0.134.0/http/server.ts'
 import router from './routes.ts'
 import Database from './database.ts'
 import { Application } from 'https://deno.land/x/oak@v10.5.1/application.ts'
+import { bold, cyan, green } from 'https://deno.land/std@0.114.0/fmt/colors.ts'
+import { isHttpError } from 'https://deno.land/x/oak@v10.5.1/mod.ts'
 
 const port = parseInt(Deno.env.get('PORT')!, 10)
 
 const database = new Database()
 
 const app = new Application()
+
+// Logger
+app.use(async (context, next) => {
+	await next()
+	const rt = context.response.headers.get('X-Response-Time')
+	console.log(
+		`${green(context.request.method)} ${cyan(
+			decodeURIComponent(context.request.url.pathname)
+		)}`
+	)
+})
+
+// Error handler
+app.use(async (context, next) => {
+	try {
+		await next()
+	} catch (err) {
+		if (isHttpError(err)) {
+			context.response.status = err.status
+			const { message, status, stack } = err
+			if (context.request.accepts('json')) {
+				context.response.body = { message, status, stack }
+				context.response.type = 'json'
+			} else {
+				context.response.body = `${status} ${message}\n\n${stack ?? ''}`
+				context.response.type = 'text/plain'
+			}
+		} else {
+			console.log(err)
+			throw err
+		}
+	}
+})
 
 app.use(router.routes())
 app.use(router.allowedMethods())
